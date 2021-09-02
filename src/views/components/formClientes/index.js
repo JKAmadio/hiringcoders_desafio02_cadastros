ff5d89146adf06aadc7039471b8b7c292ec9a05a
Julia K Amadio <jkahvedjian@gmail.com>
2021-09-02 16:40:23 -0300

    Recebendo inputs do cadastro

--- src/views/components/formClientes/index.js
+++ src/views/components/formClientes/index.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import * as S from './styled'
 
 function FormCliente(){
+
+	const [nome, setNome] = useState('')
+	const [email, setEmail] = useState('')
+	const [telefone, setTelefone] = useState('')
+	const [dataNascimento, setDataNascimento] = useState('')
+
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(nome)
+		console.log(email)
+		console.log(telefone)
+		console.log(dataNascimento)
+		console.log("Botao clicado")
+		
+	}
+
 	return(
 		<div>
-			<S.FormularioCliente autoComplete="off" onSubmit={console.log('Botao clicado')}>
+			<S.FormularioCliente autoComplete="off" onSubmit={handleSubmit}>
 				<h1>Formulario Cliente</h1>
-				<S.ClienteInput type="text" placeholder="Nome Completo"/>
-				<S.ClienteInput type="text" placeholder="seu_email@dominio.com"/>
-				<S.ClienteInput type="text" placeholder="(XX) XXXXX-XXXX"/>
-				<S.ClienteInput type="text" placeholder="DD/MM/AAAA"/>
+				<S.ClienteInput type="text" name={nome} onChange={e => setNome(e.target.value)} placeholder="Nome Completo"/>
+				<S.ClienteInput type="text" name={email} onChange={e => setEmail(e.target.value)} placeholder="seu_email@dominio.com"/>
+				<S.ClienteInput type="text" name={telefone} onChange={e => setTelefone(e.target.value)} placeholder="(XX) XXXXX-XXXX"/>
+				<S.ClienteInput type="text" name={dataNascimento} onChange={e => setDataNascimento(e.target.value)} placeholder="DD/MM/AAAA"/>
 				<S.ClienteBotao>Cadastrar</S.ClienteBotao>
 			</S.FormularioCliente>
 		</div>
