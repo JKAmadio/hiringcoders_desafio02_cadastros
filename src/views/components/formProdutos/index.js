d2c43aecb7a8dedef669f9629deeeb2dcd53b8c0
Julia K Amadio <jkahvedjian@gmail.com>
2021-09-03 19:18:30 -0300

    Formulário Produtos - estilização e lidar com submit

--- src/views/components/formProdutos/index.js
+++ src/views/components/formProdutos/index.js
@@ -1,16 +1,62 @@
-import Cliente from './contentful'
+import * as S from './styled'
+//import Cliente from './contentful'
+import { useState } from 'react'
 
 function FormProdutos () {
+	/*
 	Cliente.getEntries({
 		content_type: 'product'
 	})
 	.then(response => console.log('RESPONSE', response.items))
-	.catch(console.error)
+	.catch(console.error)*/
+
+	const [nome, setNome] = useState('')
+	const [categoria, setCategoria] = useState('')
+	const [marca, setMarca] = useState('')
+	const [quantidade, setQuantidade] = useState('')
+	const [dataCadastro, setDataCadastro] = useState('')
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log(nome)
+	}
 
 	return (
 		<div>
-		<h1>API Produtos</h1>
-	</div>
+			<S.FormularioProduto onSubmit={e => handleSubmit(e)}>
+				<h1>API Produtos</h1>
+				<S.ProdutoInput 
+					type="text" 
+					placeholder="Nome do Produto" 
+					name="nome"
+					value={nome}
+					onChange={e => setNome(e.target.value)}/>
+				<S.ProdutoSelect value={categoria} onChange={e => setCategoria(e.target.value)}>
+					<option value='cafe'>Café</option>
+					<option value='xicaras'>Xícaras/Canecas</option>
+					<option value='acessorios'>Acessórios</option>
+				</S.ProdutoSelect>
+				<S.ProdutoInput 
+					type="text" 
+					placeholder="Marca do Produto" 
+					name="marca"
+					value={marca}
+					onChange={e => setMarca(e.target.value)}/>
+				<S.ProdutoInput 
+					type="number" 
+					placeholder="Quantidade" 
+					name="quantidade"
+					value={quantidade}
+					onChange={e => setQuantidade(e.target.value)}/>
+				<S.ProdutoInput 
+					type="date" 
+					placeholder="DD/MM/AAA" 
+					name="dataCadastro"
+					value={dataCadastro}
+					onChange={e => setDataCadastro(e.target.value)}/>
+				<S.ProdutoBotao>Cadastrar</S.ProdutoBotao>
+			</S.FormularioProduto>
+		</div>
 	)
 }
 
---
+++ src/views/components/formProdutos/styled.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const FormularioProduto = styled.form`
+	display:flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-around;
+	width: 50%;
+	margin: 10vh auto;
+`
+
+export const ProdutoInput = styled.input`
+	padding: 10px;
+	margin: 10px 0;
+	width: 100%;
+	border-radius: 10px;
+	border: 1px solid #c4c4c4;
+
+	&:focus{
+		outline: none;
+	}
+`
+export const ProdutoSelect = styled.select`
+	padding: 10px;
+	margin: 10px 0;
+	width: 103%;
+	border-radius: 10px;
+	border: 1px solid #c4c4c4;
+
+	&:focus{
+		outline: none;
+	}
+` 
+
+export const ProdutoBotao = styled.button`
+	padding: 10px 20px;
+	margin: 10px 0;
+	border-radius: 10px;
+	border: 1px solid #c4c4c4;
+
+	&:hover{
+		cursor: pointer;
+		background-color: #d4d4d4;
+	}
+`
+export const TabelaProdutos = styled.table`
+	border-collapse: collapse;
+	width: 50%;
+	margin: 10vh auto;
+`
+
+export const TabelaHeader = styled.th`
+	border: 1px solid #c4c4c4;
+	padding: 5px;
+	`
+
+export const TabelaCelulas = styled.td`
+	border: 1px solid #c4c4c4;
+	padding: 5px 10px;
+	width: 25%;
+	text-align: center;
+`
