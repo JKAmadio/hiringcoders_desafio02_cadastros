9710fda1b72df45bb30d1e1d3db397efcdc4d984
Julia K Amadio <jkahvedjian@gmail.com>
2021-09-07 23:11:15 -0300

    Estilização Home

--- src/views/pages/Home/index.js
+++ src/views/pages/Home/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import * as S from './styled'
+import image from '../../../assets/server.png'
 
 export default function Home() {
 	return (
 		<S.Container>
-			<h1>Sistema de cadastro</h1>
-			<div>
-				<Link to="/clientes"><S.Botao>Cadastrar Cliente</S.Botao></Link>
-				<Link to="/produtos"><S.Botao>Cadastrar Produtos</S.Botao></Link>
-			</div>
+			<S.Title>Sistema de cadastro</S.Title>
+			<S.Body>
+				<div>
+					<Link to="/clientes"><S.Botao>Cadastrar Clientes</S.Botao></Link>
+					<Link to="/produtos"><S.Botao>Cadastrar Produtos</S.Botao></Link>
+				</div>
+				<S.Image src={image}/>
+			</S.Body>
 		</S.Container>
 	)
 }
--- src/views/pages/Home/styled.js
+++ src/views/pages/Home/styled.js
@@ -10,15 +10,35 @@ export const Container = styled.div`
 	margin: 0 auto;
 `
 
-export const Botao = styled.button`
-	padding: 10px 20px;
-	margin: 10px 0;
-	border-radius: 10px;
-	border: 1px solid #c4c4c4;
-	margin: 20px;
+export const Title = styled.h1`
+	color: #0E538C;
+	font-size: 38px;
+`
 
+export const Body = styled.div`
+	display:flex;
+	align-items: center;
+`
+
+export const Image = styled.img`
+	height: 450px;
+`
+
+export const Botao = styled.button`
+	width: 200px;
+	padding: 10px 0;
+	margin: 10px 20px;
+	border-radius: 15px;
+	border: 1px solid #0E538C;
+	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	font-weight: 500;
+	font-size: 16px;
+	background-color: #FFBE55;
+	color: #202020;
+	
 	&:hover{
 		cursor: pointer;
-		background-color: #d4d4d4;
+		background-color: #0E538C;
+		color: #ffffff;
 	}
 `
